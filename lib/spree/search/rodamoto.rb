7b6af18cd04d0cd146b648fc548a7ec03bc7be05
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-20 20:48:04 +0200

    add search base scope

--- lib/spree/search/rodamoto.rb
+++ lib/spree/search/rodamoto.rb
@@ -1,33 +1,31 @@
 module Spree::Search
   class Rodamoto < Spree::Core::Search::Base
-    def get_products_conditions_for(base_scope, query)
-      fields = [:name, :description, :sku, :tire_fr_id, :tire_speed_code_id, 
-                :tire_tttl_id, :tire_profile_id, :tire_ic_id, :tire_innertube_id, :tire_width_id]
-      values = query.split
-
-      where_str = fields.map{|field|
-        case field
-          when :sku
-            Array.new(values.size, "variants.sku LIKE ?").join(' OR ')
-          when :tire_fr_id
-            Array.new(values.size, "variants.tire_fr_id = ?").join(' OR ')
-          when :tire_speed_code_id
-            Array.new(values.size, "variants.tire_speed_code_id = ?").join(' OR ')
-          when :tire_tttl_id
-            Array.new(values.size, "variants.tire_tttl_id = ?").join(' OR ')
-          when :tire_profile_id
-            Array.new(values.size, "variants.tire_profile_id = ?").join(' OR ')
-          when :tire_ic_id
-            Array.new(values.size, "variants.tire_ic_id = ?").join(' OR ')
-          when :tire_innertube_id
-            Array.new(values.size, "variants.tire_innertube_id = ?").join(' OR ')
-          when :tire_width_id
-            Array.new(values.size, "variants.tire_width_id = ?").join(' OR ')
-          else
-          Array.new(values.size, "products.#{field} LIKE ?").join(' OR ')
-        end
-      }.join(' OR ')
-      base_scope.joins(:variants_including_master).where([where_str, values.map{|value| "%#{value}%"} * fields.size].flatten)
+    def get_base_scope
+      base_scope = @cached_product_group ? @cached_product_group.products.active : Spree::Product.active
+      base_scope = base_scope.by_width(width)
+      base_scope = base_scope.by_profile(profile)
+      base_scope = base_scope.by_innertube(innertube)
+      base_scope = base_scope.by_ic(ic)
+      base_scope = base_scope.by_speed(speed)
+      base_scope = base_scope.by_fr(fr)
+      base_scope = base_scope.by_tttl(tttl)
+      base_scope = Spree::Product.active
+      base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
+      base_scope = get_products_conditions_for(base_scope, keywords) unless keywords.blank?
+      base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
+      base_scope = add_search_scopes(base_scope)
+      base_scope
     end
+    
+    def prepare(params)
+      super
+      @properties[:tire_width_id] = params[:tire_width_id]
+      @properties[:tire_profile_id] = params[:tire_profile_id]
+      @properties[:tire_innertube_id] = params[:tire_innertube_id]
+      @properties[:tire_ic_id] = params[:tire_ic_id]
+      @properties[:tire_speed_code_id] = params[:tire_speed_code_id]
+      @properties[:tire_fr_id] = params[:tire_fr_id]
+      @properties[:tire_tttl_id] = params[:tire_tttl_id]
+    end    
   end
 end
